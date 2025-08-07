6e0e351328ee75f68b91f1b271be287f4683c635
John Stegmaier <jupdown@protonmail.com>
2025-08-07 11:14:51 -0400

    Add simple test for the case where we have 3 as an input

--- FizzBuzz.Tests/SolutionTest.cs
+++ FizzBuzz.Tests/SolutionTest.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using FizzBuzz;
 using JetBrains.Annotations;
 using Xunit;
@@ -11,6 +12,18 @@ public class SolutionTest
     [Fact]
     public void METHOD()
     {
-        
+        FizzBuzz.Solution solution = new();
+        var expected = GenerateExpectedFizzBuzzListForInputThree();
+        IList<string> actual = solution.FizzBuzz(3);
+        Assert.Equal(expected, actual);
+    }
+
+    private static IList<string> GenerateExpectedFizzBuzzListForInputThree()
+    {
+        IList<string> expected = new List<string>();
+        expected.Add("1");
+        expected.Add("2");
+        expected.Add("Fizz");
+        return expected;
     }
 }
