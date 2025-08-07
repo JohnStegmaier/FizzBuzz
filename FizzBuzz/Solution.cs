e328dd71abbc054f6095cd98ff92e5550628a06b
John Stegmaier <jupdown@protonmail.com>
2025-08-07 11:18:35 -0400

    Make test pass

--- FizzBuzz/Solution.cs
+++ FizzBuzz/Solution.cs
@@ -2,8 +2,29 @@ namespace FizzBuzz;
 
 public class Solution
 {
+    private List<string> returnValue = new List<string>();
     public IList<string> FizzBuzz(int n)
     {
-        return null;
+        for (int i = 1; i <= n; i++)
+        {
+            if (isDivisiableByThree(i))
+            {
+                returnValue.Add("Fizz");
+            }
+            else
+            {
+                returnValue.Add(i.ToString());
+            }
+        }
+        return returnValue;
+    }
+
+    private bool isDivisiableByThree(int input)
+    {
+        if (input % 3 == 0)
+        {
+            return true;
+        }
+        return false;
     }
 }
