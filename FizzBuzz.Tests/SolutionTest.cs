f3c4a60dfbe93ce212b823a34a3df28e8caaa9f9
John Stegmaier <jupdown@protonmail.com>
2025-08-07 11:09:11 -0400

    Add unit test?

---
+++ FizzBuzz.Tests/SolutionTest.cs
@@ -0,0 +1,16 @@
+using FizzBuzz;
+using JetBrains.Annotations;
+using Xunit;
+
+namespace FizzBuzz.Tests;
+
+[TestSubject(typeof(Solution))]
+public class SolutionTest
+{
+
+    [Fact]
+    public void METHOD()
+    {
+        
+    }
+}
