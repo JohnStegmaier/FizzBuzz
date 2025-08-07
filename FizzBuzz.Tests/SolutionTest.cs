2c2378faf9d6f603a481446a686fa36a339147b2
John Stegmaier <jupdown@protonmail.com>
2025-08-07 11:22:14 -0400

    Add test case for Buzz

--- FizzBuzz.Tests/SolutionTest.cs
+++ FizzBuzz.Tests/SolutionTest.cs
@@ -13,8 +13,17 @@ public class SolutionTest
     public void ShouldReturnFizzForNumberDivisibleByThree()
     {
         FizzBuzz.Solution solution = new();
-        var expected = GenerateExpectedFizzBuzzListForInputThree();
+        IList<string> expected = GenerateExpectedFizzBuzzListForInputThree();
         IList<string> actual = solution.FizzBuzz(3);
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void ShouldReturnBuzzForNumberDivisibleByFive()
+    {
+        FizzBuzz.Solution solution = new();
+        IList<string> expected = GenerateExpectedFizzBuzzListForInputFive();
+        IList<string> actual = solution.FizzBuzz(5);
         Assert.Equal(expected, actual);
     }
 
@@ -24,6 +33,17 @@ public class SolutionTest
         expected.Add("1");
         expected.Add("2");
         expected.Add("Fizz");
+        return expected;
+    }
+    
+    private static IList<string> GenerateExpectedFizzBuzzListForInputFive()
+    {
+        IList<string> expected = new List<string>();
+        expected.Add("1");
+        expected.Add("2");
+        expected.Add("Fizz");
+        expected.Add("4");
+        expected.Add("Buzz");
         return expected;
     }
 }
